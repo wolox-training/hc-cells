0420b4eccf4a1d79f29c3c5e5f54417c30d861a6
haylerjair.caicedo.contractor.cidentity <haylerjair.caicedo.contractor.cidentity@bbva.com>
2022-10-19 09:14:42 -0500

    Add files card.js and card.styles.js

---
+++ src/card/card.js
@@ -0,0 +1,51 @@
+import { LitElement, html } from "lit-element";
+import styles from "./card.styles";
+
+
+class CardComp extends LitElement {
+
+  static get styles() {
+    return [styles];
+  }
+
+  static get properties() {
+    return {
+      cover: { type: Object }
+    }
+  }
+
+  constructor() {
+    super();
+    this.cover = {
+      sprites: {},
+      author: "",
+      book_title: []
+    };
+  }
+
+  render() {
+    
+    return html` 
+
+      <div class="body-list">
+            <div class="container">
+              <div class="list-wrapper">
+                <div class="list-cards">
+                  <div class="list-card">
+                    <div class="list-cover">
+                      <img class="img-cover" src="${this.cover.sprites?.img}" alt="${this.cover.author}">
+                      <h2 class="h2">${this.cover.author}</h2>
+                      <h1 class="h1">${this.cover.book_title}</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+      </div>
+      
+    `;
+  }
+}
+
+customElements.define("book-comp", CardComp);
+
---
+++ src/card/card.styles.js
@@ -0,0 +1,61 @@
+import { css } from "lit-element";
+
+export default css`
+  .body-search { 
+      display: flex;
+      justify-content: center;
+    }
+    
+  .list-wrapper {
+    align-items: center;
+    display: flex;
+    flex-grow: 1;
+    justify-content: center;
+    width: 100%;
+  }
+
+  .list-cards {
+    align-items: center;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    max-width: 936px;
+    width: 100%;
+  }
+
+  .list-card {
+    background: white;
+    border: solid;
+    box-shadow: 4px 4px 10px 0 rgba($black, 0.5);
+    display: flex;
+    flex-flow: column;
+    margin: 16px;
+    max-height: 300px;
+    max-width: 198px;
+    padding: 28px 28px 24px;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+
+  .img-cover{
+    margin-bottom: 12px;
+    max-width: 142px;
+    max-height: 200px;
+    
+  }
+
+  .h1 {
+    color: gray;
+    font-size: 14px;
+  }
+
+  .h2 {
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 8px;
+  }
+`; 
+
