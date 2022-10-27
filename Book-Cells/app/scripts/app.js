d09011873be2a0573d4c3535f0bd2aa6fd061b94
haylerjair.caicedo.contractor.cidentity <haylerjair.caicedo.contractor.cidentity@bbva.com>
2022-10-27 09:43:29 -0500

    Add app.js and lit-initial-components.js about scripts

--- Book-Cells/app/scripts/app.js
+++ Book-Cells/app/scripts/app.js
@@ -3,10 +3,10 @@
 
   window.CellsPolymer.start({
     routes: {
-      'login': '/',
-      'dashboard': '/dashboard',
-      'movement-detail': '/movement/:id/:label',
-      'help': '/help'
+      'login':'/',
+      'genesis': '/genesis',
+      'card':'/detail',
+      
     }
   });
 }());
